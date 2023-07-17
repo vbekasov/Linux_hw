8c6a518f71979781c9245fe03a3d9bc4a761ce15
vbekasov <49273219+vbekasov@users.noreply.github.com>
2023-07-17 15:49:24 +0200

    Create vb-hw2.sh

---
+++ vb-hw2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PTH="/home/user/start/";
+mkdir -p $PTH
+touch $PTH"1.txt";
+echo "Start" >> $PTH"1.txt";
+date >> $PTH"1.txt";
+ls $PTH"1.txt";
+cat $PTH"1.txt";
+PTH1="/opt/task/";
+mkdir -p $PTH1;
+cp $PTH"1.txt" $PTH1"start.txt";
+ls -a $PTH1;
+ls -al / | tail -4 >> $PTH1"start.txt";
+cat $PTH1"start.txt";
